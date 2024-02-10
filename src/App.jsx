e81294b3b466dde85ecce86d4ca71239592b485a
Lewis Stopford <l.stopford@icloud.com>
2024-02-10 23:42:28 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ScrollSection from "./Components/ScrollSection/ScrollSection"
+
+function App() {
+
+  return (
+    <>
+      <ScrollSection title='Hello' colour='red' />
+      <ScrollSection title='This is' colour='blue' />
+      <ScrollSection title='Animating on scroll' colour='orange' />
+      <ScrollSection title='Its really easy' colour='yellow' />
+      <ScrollSection title='When using framer motion' colour='green' />
+      <ScrollSection title='Idk how to do it without it' colour='lime' />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ScrollSection/ScrollSection.jsx
@@ -0,0 +1,26 @@
+import styles from './ScrollSection.module.css';
+import { motion } from 'framer-motion';
+
+function ScrollSection({ title, colour }){
+    return(
+        <div className={styles.scrollSection} style={{ backgroundColor: colour }}>
+            <motion.h1
+            initial={{
+                opacity: 0,
+                x: '-50px',
+            }}
+            whileInView={{
+                opacity: 1,
+                x: '0px',
+                margin: '20px 0px 0px 0px',
+            }}
+            transition={{
+                type: 'spring',
+                stiffness: 400,
+                damping: 20,
+                delay: 0.25,
+            }}>{title}</motion.h1>
+        </div>
+    );
+}
+export default ScrollSection;
