f8606c17e05f510f80533b160979bc2e7250ca13
Lewis Stopford <l.stopford@icloud.com>
2024-02-11 22:17:10 +0000

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/Pages/Home/Home.jsx';
 import AnimateOnScroll from "./Components/Pages/animate-on-scroll/AnimateOnScroll.jsx";
 
