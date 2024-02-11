4c69c33caf2ac529e18772b9b7c9cf91eb25ebbc
Lewis Stopford <l.stopford@icloud.com>
2024-02-11 22:20:47 +0000

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/Pages/Home/Home.jsx';
 import AnimateOnScroll from "./Components/Pages/animate-on-scroll/AnimateOnScroll.jsx";
 
 function App() {
   return(
-    <Router>
+    <Home />
+  );
+}
+export default App
+
+/*<Router>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/home' element={<Home />}/>
         <Route path='/animate-on-scroll' element={<AnimateOnScroll />}/>
       </Routes>
-    </Router>
-  );
-}
-export default App
+    </Router>*/
