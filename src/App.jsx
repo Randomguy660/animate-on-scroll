eb845048c6df1a0bfe685e790e98aa26d9e8a2e4
Lewis Stopford <l.stopford@icloud.com>
2024-02-11 21:50:07 +0000

    Home page fix finally maybe

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Components/Pages/Home/Home.jsx';
 import AnimateOnScroll from "./Components/Pages/animate-on-scroll/AnimateOnScroll.jsx";
 
 function App() {
   return(
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path='/' component={Home}/>
         <Route path='/home' component={Home}/>
         <Route path='/animate-on-scroll' component={AnimateOnScroll}/>
-      </Switch>
+      </Routes>
     </Router>
   );
 }
